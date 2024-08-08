a872b3877112b70d135a52149b6c7f03534cecbb
John <167108058+proweavernsdev@users.noreply.github.com>
2024-08-09 06:34:07 +0800

    New Ionic project

---
+++ speedy/src/views/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Home</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="ion-padding">
+        <div id="container">
+          <strong>Welcome to Speedy</strong>
+          <p>
+            A simple app to help you get started with Ionic and Vue.
+          </p>
+          <ion-button expand="full" @click="goToLogin()">Login</ion-button>
+        </div>
+      </ion-content>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const goToLogin = () => {
+  router.push('/login');
+};
+
+</script>
+
+<style scoped></style>
---
+++ speedy/src/views/components/LoginComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <ion-page>
+        <ion-content :fullscreen="true" class="ion-padding">
+            <ion-card class="ion-padding">
+                <ion-card-header>
+                    <ion-card-title>Login</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                    <ion-item>
+                        <ion-label position="floating">Email</ion-label>
+                        <ion-input v-model="email"></ion-input>
+                    </ion-item>
+                    <ion-item>
+                        <ion-label position="floating">Password</ion-label>
+                        <ion-input type="password" v-model="password"></ion-input>
+                    </ion-item>
+                    <ion-button expand="full" @click="login()">Login</ion-button>
+                </ion-card-content>
+                <ion-card-subtitle
+                    class="tw-flex tw-justify-center tw-items-center tw-w-full tw-border-2 gap-2 text-black">
+                    <a href="#">Forgot Password?</a>
+                    <a href="#">Register</a>
+                </ion-card-subtitle>
+            </ion-card>
+        </ion-content>
+    </ion-page>
+</template>
+<script setup lang="ts">
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const email = ref('');
+const password = ref('');
+const router = useRouter();
+const login = () => {
+    router.push('/home');
+};
+</script>
+
+<style scoped></style>
